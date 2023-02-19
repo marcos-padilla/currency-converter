10b81bd3235b389f3341e638774503b5693982e9
Marcos <marcosalejandro.pd@gmail.com>
2023-02-19 10:20:40 -0500

    Added the design of the box with the input

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import currency from './mock/latest-currency.json'
 export default function App() {
 	return (
 		<div>
-			<select>
-				{Object.keys(currency.rates).map((key) => {
-					return (
-						<option key={key} value={key}>
-							{key}
-						</option>
-					)
-				})}
-			</select>
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					gap: 10,
+					border: '2px solid darkgray',
+					padding: 20,
+					borderRadius: 10,
+				}}
+			>
+				<select style={{ textAlign: 'center' }}>
+					{Object.keys(currency.rates).map((key) => {
+						return (
+							<option key={key} value={key}>
+								{key}
+							</option>
+						)
+					})}
+				</select>
+				<input type='number' style={{ textAlign: 'center' }} />
+			</div>
 		</div>
 	)
 }
