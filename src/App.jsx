9afd834697318da827017da1dc59e2599fcce505
Marcos <marcosalejandro.pd@gmail.com>
2023-02-19 10:15:53 -0500

    Added select for all diferents rates

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import currency from './mock/latest-currency.json'
 
-export default function App(){
-  
-
+export default function App() {
+	return (
+		<div>
+			<select>
+				{Object.keys(currency.rates).map((key) => {
+					return (
+						<option key={key} value={key}>
+							{key}
+						</option>
+					)
+				})}
+			</select>
+		</div>
+	)
 }
