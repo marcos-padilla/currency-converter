1d36ae20eb14ef8ddb4953e6cc3fb0b5c58c3a78
Marcos <marcosalejandro.pd@gmail.com>
2023-02-20 13:36:28 -0500

    Fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import CurrencyForm from './CurrencyForm'
 import { useCallback, useEffect, useState } from 'react'
-import currencyData from './mock/latest-currency.json'
+import API_URL from './API_URL'
 
 export default function App() {
+	const [currencyData, setCurrencyData] = useState(null)
+
 	const [fromCurrency, setFromCurrency] = useState('USD')
 	const [toCurrency, setToCurrency] = useState('EUR')
 
 	const [fromValue, setFromValue] = useState(0)
 	const [toValue, setToValue] = useState(0)
 
+	useEffect(() => {
+		fetch(API_URL)
+			.then((resp) => resp.json())
+			.then((resp) => {
+				setCurrencyData(resp)
+			})
+			.catch((err) => {
+				console.log({ err })
+			})
+	}, [])
 	const calculateCurrency = useCallback(
 		({ rates, fromCurrency, toCurrency, amount }) => {
 			var rates = currencyData.rates
@@ -35,43 +47,47 @@ export default function App() {
 	}, [fromCurrency, fromValue])
 
 	return (
-		<div style={{ display: 'flex', gap: 20 }}>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					placeItems: 'center',
-					gap: 10,
-				}}
-			>
-				<span>From</span>
-				<CurrencyForm
-					currency={fromCurrency}
-					setCurrency={setFromCurrency}
-					value={fromValue}
-					setValue={setFromValue}
-					currencyData={currencyData}
-					editable
-				/>
-			</div>
-			<div
-				style={{
-					display: 'flex',
-					flexDirection: 'column',
-					placeItems: 'center',
-					gap: 10,
-				}}
-			>
-				<span>To</span>
-				<CurrencyForm
-					currency={toCurrency}
-					setCurrency={setToCurrency}
-					value={toValue}
-					setValue={() => {}}
-					currencyData={currencyData}
-					editable={false}
-				/>
-			</div>
+		<div>
+			{currencyData && (
+				<div style={{ display: 'flex', gap: 20 }}>
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							placeItems: 'center',
+							gap: 10,
+						}}
+					>
+						<span>From</span>
+						<CurrencyForm
+							currency={fromCurrency}
+							setCurrency={setFromCurrency}
+							value={fromValue}
+							setValue={setFromValue}
+							currencyData={currencyData}
+							editable
+						/>
+					</div>
+					<div
+						style={{
+							display: 'flex',
+							flexDirection: 'column',
+							placeItems: 'center',
+							gap: 10,
+						}}
+					>
+						<span>To</span>
+						<CurrencyForm
+							currency={toCurrency}
+							setCurrency={setToCurrency}
+							value={toValue}
+							setValue={() => {}}
+							currencyData={currencyData}
+							editable={false}
+						/>
+					</div>
+				</div>
+			)}
 		</div>
 	)
 }
